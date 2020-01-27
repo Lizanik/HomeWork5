3b7ea01d965027dc19f7c7411ffcde6cb4398e98
Liza Nikitenko <59203054+Lizanik@users.noreply.github.com>
2020-01-27 13:44:29 +0200

    Update main2.java

--- src/com/epam/tasks/main2.java
+++ src/com/epam/tasks/main2.java
@@ -2,19 +2,25 @@ package com.epam.tasks;
 
 import java.util.Arrays;
 public class main2 {
-    public static void main(String[] args) {
-        int[] arr = new int[11];
-        int[] arr2 = new int[11];
-        System.out.println("Первый массив");
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = (int) (-10 + Math.random() * 20);
-            System.out.print(arr[i] + "\t");
+        static int[] make() {
+            int[] arr = new int[11];
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = (int) (-10 + Math.random() * 20);
+            }
+            return arr;
         }
-        System.out.println();
-        System.out.println("Второй массив");
-        for (int i = 0; i < arr2.length; i++) {
-            arr2[i] = (int) (-10 + Math.random() * 20);
-            System.out.print(arr2[i] + "\t");
+        static int [] sum(int[] arr1, int[] arr2) {
+            int[] arr = new int[arr1.length];
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = arr1[i] + arr2[i];
+            }
+            return arr;
+        }
+        public static void main(String[]args) {
+            int[] arr1 = make();
+            int[] arr2 = make();
+            System.out.println("First array" + Arrays.toString (arr1));
+            System.out.println("Second aray" +Arrays.toString( arr2));
+            System.out.println("Sum " + Arrays.toString (sum (arr1, arr2)));
         }
     }
-}
